f9456419d86564e59b205532fa64b5cccd618a81
Rafał Brzoska <rbrzoska@future-processing.com>
2020-01-28 09:59:03 +0100

    subjects

--- fp-proj/src/app/home/home.component.ts
+++ fp-proj/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject, BehaviorSubject, ReplaySubject, AsyncSubject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  subj = new Subject();
+  bSubj = new BehaviorSubject('b1');
+  rSubj = new ReplaySubject(2);
+  aSubj = new AsyncSubject();
+
   constructor() { }
 
   ngOnInit() {
+
+    this.subj.next(1);
+    this.subj.subscribe(x => console.log(x));
+    this.subj.next(2);
+
+
+    this.bSubj.next('b2');
+    this.bSubj.subscribe(x => console.log(x));
+
+    this.rSubj.next('r1');
+    this.rSubj.next('r2');
+    this.rSubj.next('r3');
+    this.rSubj.subscribe(x => console.log(x));
+
+    this.aSubj.next('a1');
+    this.aSubj.subscribe(x => console.log(x));
+    this.aSubj.next('a2');
+    this.aSubj.next('a3');
+    this.aSubj.complete();
   }
 
 }
