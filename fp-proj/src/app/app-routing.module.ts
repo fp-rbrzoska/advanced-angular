8bc10dfe7c13d61b504c69d2ec6f76d24de3f88f
Rafał Brzoska <rbrzoska@future-processing.com>
2020-01-29 08:46:40 +0100

    users module

--- fp-proj/src/app/app-routing.module.ts
+++ fp-proj/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule)},
   { path: 'test', loadChildren: () => import('./test/test.module').then(m => m.TestModule)}
 ];
 
--- fp-proj/src/app/users/users.module.ts
+++ fp-proj/src/app/users/users.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from './users/users.component';
 import { UsersListComponent } from './users-list/users-list.component';
+import { Routes, RouterModule } from '@angular/router';
 
-
+const routes: Routes = [
+  { path: '', component: UsersComponent }
+];
 
 @NgModule({
   declarations: [UsersComponent, UsersListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class UsersModule { }
