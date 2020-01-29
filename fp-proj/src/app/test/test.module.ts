0520a0bd6ba500e1de70d06186c5e45ad8712847
Rafał Brzoska <rbrzoska@future-processing.com>
2020-01-29 08:31:56 +0100

    routing test

--- fp-proj/src/app/test/test.module.ts
+++ fp-proj/src/app/test/test.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TestComponent } from './test/test.component';
+import { Routes, RouterModule } from '@angular/router';
 
-
+const routes: Routes = [
+  { path: '', component: TestComponent }
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [TestComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class TestModule { }
